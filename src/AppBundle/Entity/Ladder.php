39f89d6870cf32953b53a189f4f8fbf2145b5af4
Greg Dabrowski <gregory.dabrowski@gmail.com>
2017-07-09 10:04:11 +0000

    add ladder entity

---
+++ src/AppBundle/Entity/Ladder.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Ladder
+ *
+ * @ORM\Table(name="ladder")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\LadderRepository")
+ */
+class Ladder
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="game", type="string", length=255, nullable=true)
+     */
+    private $game;
+
+    /**
+     * @var array
+     *
+     * @ORM\Column(name="players", type="array", nullable=true)
+     */
+    private $players;
+
+    /**
+     * @var \stdClass
+     *
+     * @ORM\Column(name="rules", type="object")
+     */
+    private $rules;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Ladder
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set game
+     *
+     * @param string $game
+     *
+     * @return Ladder
+     */
+    public function setGame($game)
+    {
+        $this->game = $game;
+
+        return $this;
+    }
+
+    /**
+     * Get game
+     *
+     * @return string
+     */
+    public function getGame()
+    {
+        return $this->game;
+    }
+
+    /**
+     * Set players
+     *
+     * @param array $players
+     *
+     * @return Ladder
+     */
+    public function setPlayers($players)
+    {
+        $this->players = $players;
+
+        return $this;
+    }
+
+    /**
+     * Get players
+     *
+     * @return array
+     */
+    public function getPlayers()
+    {
+        return $this->players;
+    }
+
+    /**
+     * Set rules
+     *
+     * @param \stdClass $rules
+     *
+     * @return Ladder
+     */
+    public function setRules($rules)
+    {
+        $this->rules = $rules;
+
+        return $this;
+    }
+
+    /**
+     * Get rules
+     *
+     * @return \stdClass
+     */
+    public function getRules()
+    {
+        return $this->rules;
+    }
+}
+
